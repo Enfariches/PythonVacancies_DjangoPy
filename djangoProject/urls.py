e551899d21ba625f8a2ec1628dadf87a1b24c08f
Sergey <sergey.pervukhin@urfu.me>
2023-01-12 22:49:18 +0500

    Приложение 12.01 (2 commit)

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from app1.views import index_page, demand_page, geography_page, skills_page, lastvacancy_page
 
 # from app1.views import base
@@ -27,3 +30,7 @@ urlpatterns = [
     path('skills', skills_page),
     path('lastvacancy', lastvacancy_page),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
